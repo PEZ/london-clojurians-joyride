728346dbcd25a49f08d88f712991803b7a15cc88
Peter Strömberg <pez@pezius.com>
2022-11-25 11:14:18 +0100

    Update playground

--- scrap.cljs
+++ scrap.cljs
@@ -6,3 +6,15 @@
   (.appendLine (joy/output-channel) "Opening education")
   (vscode/commands.executeCommand "simpleBrowser.show"
                                   (str "https://" (+ 0.1 0.2) ".com")))
+
+(require '[promesa.core :as p])
+(p/let [greetee (vscode/window.showInputBox
+                 #js {:title "Joyride Greeter"
+                      :prompt "Who do you want to greet today?"
+                      :placeHolder "My wife. All my patrons. Mahatma Gandhi..."})
+        button (vscode/window.showInformationMessage (str "Hello " greetee "! ♥️") "OK" "Fabulous")
+        right-back-at-you (if button
+                            button
+                            "boring")]
+  (vscode/window.showInformationMessage 
+   (str "You are " right-back-at-you "! 😀")))
